50ad6a3ffd9055215256bd8787d9cf78a1d02457
AnaLJTP <bryasysfeyre@gmail.com>
2023-01-27 05:49:58 -0500

    Agregar archivos de proyecto.

---
+++ SueldoEmpleado.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniSueldo
+{
+    public partial class SueldoEmpleado : Form
+    {
+        public SueldoEmpleado()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2CustomGradientPanel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2CustomGradientPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
